80f8585dea6e54e3b26508a288c16450f814f879
Paweł Grzegorek <pgrzegorek41@gmail.com>
2019-12-18 17:20:12 +0100

    Score Component

---
+++ src/Components/Score.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Fade from "react-reveal";
+
+function Score({score, level}) {
+    return (
+        <div className='score'>
+            <Fade right duration={1500}>
+                <h1>Score: {score}</h1>
+                <h1>Level: {level}</h1>
+            </Fade>
+        </div>
+    )
+}
+
+export default Score
+
