9fcbe3213d2cb1e20d1c02653c2eb4604554f552
Paweł Grzegorek <pgrzegorek41@gmail.com>
2019-12-19 13:08:29 +0100

    App component

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,33 @@
 import React, {useState, useEffect} from 'react';
+import WelcomeWindow from './WelcomeWindow';
+import Game from './Game';
+import GameOver from './GameOver';
 
 function App() {
+    const [score, setScore] = useState(0);
+    const [welcomeWindow, setWelcomeWindow] = useState(true);
+    const [appWindow, setAppWindow] = useState(false);
+    const [gameOverWindow, setGameOverWindow] = useState(false);
+    const [userName, setUserName] = useState('GallAnonim');
+    const [bestScores, setBestScores] = useState([]);
+
+    const readyToGame = () => {
+        setWelcomeWindow(false);
+        setAppWindow(true);
+        setGameOverWindow(false);
+    };
+    const gameOver = () => {
+        setWelcomeWindow(false);
+        setAppWindow(false);
+        setGameOverWindow(true);
+    };
+
   return (
-    <div className="App">
-      <p>Hello World</p>
-    </div>
+      <div className="App">
+          {welcomeWindow && <WelcomeWindow close={readyToGame} userName={setUserName}/>}
+          {appWindow && <Game gameover={gameOver} score={setScore} userName={userName} highScores={setBestScores}/>}
+          {gameOverWindow && <GameOver score={score} ready={readyToGame} bestScores={bestScores}/>}
+      </div>
   );
 }
 
