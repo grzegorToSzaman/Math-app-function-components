7608d9a2545b847e230087758748d7bb069e0c5a
Paweł Grzegorek <pgrzegorek41@gmail.com>
2019-12-19 13:42:37 +0100

    Operation component

---
+++ src/Components/Operation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Operation({a, b}) {
+    return (
+        <div className='operation'>
+            {a} + {b}
+        </div>
+    )
+}
+
+export default Operation;
